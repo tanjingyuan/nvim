6e6ddcbdb7827d6558324963d31f844600c06a0c
tanjy2 <tanjy2@xiaopeng.com>
2025-07-15 21:10:29 +0800

    Feat: Make sure install format and language-server 1. See as title

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,34 @@
 return {
-  "stevearc/conform.nvim",
-  event = "VeryLazy",
-  opts = function(_, opts)
-    opts.formatters_by_ft = {
-      lua = { "stylua" },
-      -- fish = { "fish_indent" },
-      sh = { "shfmt" },
-      cpp = { "clang-format" },
-      c = { "clang-format" },
-      cmake = { "cmake-format" },
-      yaml = { "yamlfix" },
-      python = { "black" },
-    }
-    return opts
-  end,
+  {
+    "stevearc/conform.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.formatters_by_ft = {
+        lua = { "stylua" },
+        -- fish = { "fish_indent" },
+        sh = { "shfmt" },
+        cpp = { "clang-format" },
+        c = { "clang-format" },
+        cmake = { "cmake-format" },
+        yaml = { "yamlfix" },
+        python = { "black" },
+      }
+      return opts
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "shfmt",
+        "clang-format",
+        "cmakelang", -- 包含 cmake-format
+        "yamlfix",
+        "black",
+        "prettier", -- 可选：用于 json, markdown 等
+      })
+    end,
+  },
 }
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "bashls",
+      -- "bazelrc",
+      "clangd",
+      "cmake",
+      "jsonls",
+      "lua_ls",
+      "marksman",
+      "pyright",
+      "taplo",
+      "yamlls",
+    },
+  },
+}
