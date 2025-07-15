d2c8c4d3771864b0cb5f26c6effadf5d6490fffd
tanjy2 <tanjy2@xiaopeng.com>
2025-07-15 14:58:20 +0800

    Chore: Modify highlight with bracket and line
    1.See as title

--- lua/utils/highlight.lua
+++ lua/utils/highlight.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
   vim.cmd([[
-      highlight MatchParen guifg=#fdf6e3 ctermbg=12 guibg=#839496
+      highlight MatchParen guifg=#ffb6c1 guibg=#ff5555 cterm=bold gui=bold
     ]])
 
   vim.cmd([[
@@ -26,6 +26,12 @@ augroup END
 
   -- 设置光标的颜色默认为拉姆的发(fà)色
   vim.cmd("highlight Cursor gui=NONE guifg=bg guibg=#ffb6c1")
+
+  -- 设置当前行行号为醒目的橙色
+  vim.cmd("highlight CursorLineNr guifg=#ff8800 gui=bold")
+
+  -- 可选：设置普通行号颜色为稍微暗一些的颜色以形成对比
+  vim.cmd("highlight LineNr guifg=#6b7280")
 end
 
 return M
