062020dec33ac1062ca38b5abbf06aec6ac4226a
tanjingyuan <wuxiao@sensetime.com>
2025-10-11 17:29:09 +0800

    Fix: Adaptive recognition shell with toggleterm 1. See as title

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,11 +2,18 @@ return {
   "akinsho/nvim-toggleterm.lua",
   event = "VeryLazy",
   config = function()
+    local user_shell = vim.env.SHELL or "/bin/bash"
+    vim.opt.shell = user_shell
+    vim.opt.shellcmdflag = "-ic"
+    local shell_with_login = user_shell .. " -l"
     require("toggleterm").setup({
       direction = "horizontal",
       on_open = function(term)
         vim.cmd("startinsert!")
       end,
+      shell = shell_with_login,
+      start_in_insert = true,
+      close_on_exit = true,
     })
   end,
   cmd = "ToggleTerm",
